fb6c5379939d99d9a27cecf416047710fd15fec5
Adithya Pai B <paiadithya26@gmail.com>
2022-02-21 20:26:41 +0530

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -4,7 +4,7 @@ uint16 print(unsigned char ch)
 {
   uint16 ax = GREEN;
   uint8  al = 0;
-  ax <<= 8;
+  ax <<= 8; // left shift 
   return (ax|= al = ch);
 }
 
@@ -12,7 +12,7 @@ uint16 print(unsigned char ch)
 void kernel_entry()
 {
   //A String That is to Be Printed on to the Console
-  char str[] = "Hello World";
+  char str[] = "Hello World : )";
 
   // Pointer to the OUTPUT buffer;  0xB8000 is the address of vga buffer
   uint16* vga_buffer = (uint16*)0xB8000;
@@ -20,7 +20,6 @@ void kernel_entry()
   //Clears the output buffer from any garbage values
   for(uint32 i = 0; i < BUFSIZE; i++)
       (vga_buffer)[i] = print(NULL);
-
   // Adds the string items to the vga buffer charecter by charecter
   for(uint32 i=0;str[i]!='\0';i++)
        vga_buffer[i] = print(str[i]);
